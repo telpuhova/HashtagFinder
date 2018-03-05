d427c9c43d1a871bbccbedbd8aa9861ac0e33d60
Kate Trahan and Natalia Telpukhova <kate@>
2018-03-05 08:47:48 -0800

    add firebase

---
+++ src/app/api-key.ts
@@ -0,0 +1,8 @@
+var config = {
+   apiKey: "AIzaSyABva5EdhZmyvgGn6UWZomLOqtbpZuDBGs",
+   authDomain: "hashtagfinder-94005.firebaseapp.com",
+   databaseURL: "https://hashtagfinder-94005.firebaseio.com",
+   projectId: "hashtagfinder-94005",
+   storageBucket: "",
+   messagingSenderId: "241041097780"
+ };
