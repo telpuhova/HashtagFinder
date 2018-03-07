a7de0eacee4e5a85552219e1548866547e58d2b7
Byron Chang <thebyronc@gmail.com>
2018-03-07 11:15:03 -0800

    remove pipe

--- client/src/app/trend-volume.pipe.ts
+++ client/src/app/trend-volume.pipe.ts
@@ -8,13 +8,13 @@ import { Trend } from './trend.model';
 export class TrendVolumePipe implements PipeTransform {
 
   transform(input: Trend[], volume) {
-    var output: Trend[] = [];
-    for (var i = 0; i < input.length; i++) {
-      if (input[i].tweet_volume != null) {
-        output.push(input[i]);
-      }
-    }
-    return output;
+    // var output: Trend[] = [];
+    // for (var i = 0; i < input.length; i++) {
+    //   if (input[i].tweet_volume != null) {
+    //     output.push(input[i]);
+    //   }
+    // }
+    // return output;
   }
 
 }
