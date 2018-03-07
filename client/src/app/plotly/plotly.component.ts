90f3673e910273565e4a8e4ee875db85fe76ec2c
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-03-07 13:18:47 -0800

    add plotly.plot

--- client/src/app/plotly/plotly.component.ts
+++ client/src/app/plotly/plotly.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Input, Output, OnInit, ElementRef, ViewChild} 
 import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
 import { TrendsComponent } from '../trends/trends.component';
-import Plotly from 'plotly.js/lib/core';
+import { Plotly} from 'plotly.js/lib/core';
 import { ChartService} from '../chart.service';
 import * as _ from 'lodash';
+// import {Plotly} from './plotly.typings.d'
 
-declare var Plotly: any;
+// declare var Plotly: any;
 
 
 @Component({
@@ -22,61 +23,72 @@ export class PlotlyComponent implements OnInit {
   constructor(private chartService: ChartService) { }
 
   ngOnInit() {
-    this.chartService.getData('rankings')
-        // .take(1)
-        .subscribe(data => {
-          Plotly.purge(this.el.nativeElement)
-          this.ternaryChart(data)
-    })
+    this.basicChart()
   }
 
-
-
-
-  ternaryChart(data) {
+  basicChart() {
     const element = this.el.nativeElement
-    const formattedData = [{
-    type: 'scatterternary',
-      mode: 'markers',
-      a: data.map(d => d.analyst ),
-      b: data.map(d => d.designer),
-      c: data.map(d => d.developer),
-      text: data.map(d => d.label),
-      marker: {
-          symbol: 100,
-          color: '#DB7365',
-          size: 14,
-          line: { width: 2 }
-      },
+    console.log(element)
+
+    const data = [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16]
     }]
+
     const style = {
-    ternary: {
-      sum: 100,
-      aaxis: this.makeAxis('Analyst', 0),
-      baxis: this.makeAxis('Developer', 45),
-      caxis: this.makeAxis('Designer', -45),
-      bgcolor: '#fff1e0'
+      margin: { t: 0 }
     }
-  }
 
-    console.log(Plotly);
-
-    Plotly.plot(element, formattedData, style);
+    Plotly.plot(element, data, style)
 
   }
-  private makeAxis(title, tickangle) {
-      return {
-        title: title,
-        titlefont: { size: 20 },
-        tickangle: tickangle,
-        tickfont: { size: 15 },
-        tickcolor: 'rgba(0,0,0,0)',
-        ticklen: 5,
-        showline: true,
-        showgrid: true
-      };
 
-}
+
+
+  // ternaryChart(data) {
+  //   const element = this.el.nativeElement
+  //   const formattedData = [{
+  //   type: 'scatterternary',
+  //     mode: 'markers',
+  //     a: data.map(d => d.analyst ),
+  //     b: data.map(d => d.designer),
+  //     c: data.map(d => d.developer),
+  //     text: data.map(d => d.label),
+  //     marker: {
+  //         symbol: 100,
+  //         color: '#DB7365',
+  //         size: 14,
+  //         line: { width: 2 }
+  //     },
+  //   }]
+  //   const style = {
+  //   ternary: {
+  //     sum: 100,
+  //     aaxis: this.makeAxis('Analyst', 0),
+  //     baxis: this.makeAxis('Developer', 45),
+  //     caxis: this.makeAxis('Designer', -45),
+  //     bgcolor: '#fff1e0'
+  //   }
+  // }
+
+  //   console.log(Plotly);
+  //
+  //   Plotly.plot(element, formattedData, style);
+  //
+  // }
+//   private makeAxis(title, tickangle) {
+//       return {
+//         title: title,
+//         titlefont: { size: 20 },
+//         tickangle: tickangle,
+//         tickfont: { size: 15 },
+//         tickcolor: 'rgba(0,0,0,0)',
+//         ticklen: 5,
+//         showline: true,
+//         showgrid: true
+//       };
+//
+// }
 }
 
 
