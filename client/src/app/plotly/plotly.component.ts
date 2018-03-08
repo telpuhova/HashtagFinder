cdf7cc5ac41c40b3c67273ca241e0f74596407e8
Guest User <Guest@Epicodus-3A.local>
2018-03-08 11:17:33 -0800

    add bar graph data

--- client/src/app/plotly/plotly.component.ts
+++ client/src/app/plotly/plotly.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { ChartService} from '../chart.service'
+import { TwitterService} from '../twitter.service'
 import * as Plotly from 'plotly.js';
 
 @Component({
   selector: 'app-plotly',
   templateUrl: './plotly.component.html',
   styleUrls: ['./plotly.component.css'],
-  providers: [ChartService]
+  providers: [TwitterService]
 })
 export class PlotlyComponent implements OnInit {
 
   @ViewChild('chart') el: ElementRef;
 
-  constructor(private chartService: ChartService) { }
+  constructor(private twitterService: TwitterService) { }
 
   ngOnInit() {
     this.basicChart()
@@ -20,12 +20,21 @@ export class PlotlyComponent implements OnInit {
 
 
   basicChart() {
-    //const element = this.el.nativeElement;
+    let graphArray = this.twitterService.getTopTags();
+    const element = this.el.nativeElement;
+    console.log("grapharray: "+graphArray);
+    // var trace1 = {};
+    // trace1["x"]= graphArray[0];
+    // trace1["y"] = graphArray[1];
+    // trace1["type"] = 'bar';
+    // trace1["marker"] = {
+    //   color: 'rgb(142,124,195)'
+    // };
+    // console.log(trace1);
     var trace1 = {
-      x: ['Liam', 'Sophie', 'Jacob', 'Mia', 'William', 'Olivia'],
-      y: [8.0, 8.0, 12.0, 12.0, 13.0, 20.0],
+      x: graphArray[0],
+      y: graphArray[1],
       type: 'bar',
-      text: ['4.17 below the mean', '4.17 below the mean', '0.17 below the mean', '0.17 below the mean', '0.83 above the mean', '7.83 above the mean'],
       marker: {
         color: 'rgb(142,124,195)'
       }
@@ -34,21 +43,10 @@ export class PlotlyComponent implements OnInit {
     var data = [trace1];
 
     var layout = {
-      title: 'Number of Graphs Made this Week',
-      font:{
-        family: 'Raleway, snas-serif'
-      },
-      showlegend: false,
-      xaxis: {
-        tickangle: -45
-      },
-      yaxis: {
-        zeroline: false,
-        gridwidth: 2
-      },
-      bargap :0.05
+      title: 'Popularity',
+      autorange: true
     };
 
-    Plotly.newPlot('chart', data, layout);
+    Plotly.newPlot(element, data, layout);
   }
 }
