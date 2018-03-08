a0081b058bc53493e0c85bbcc876a5cc6837126e
Byron Chang <thebyronc@gmail.com>
2018-03-08 11:56:03 -0800

    add new styling to nav

--- client/src/app/plotly/plotly.component.ts
+++ client/src/app/plotly/plotly.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { ChartService} from '../chart.service'
+import { TwitterService} from '../twitter.service'
 import * as Plotly from 'plotly.js';
-
+import { bar } from 'plotly.js';
 @Component({
   selector: 'app-plotly',
   templateUrl: './plotly.component.html',
   styleUrls: ['./plotly.component.css'],
-  providers: [ChartService]
+  providers: [TwitterService]
 })
 export class PlotlyComponent implements OnInit {
 
   @ViewChild('chart') el: ElementRef;
 
-  constructor(private chartService: ChartService) { }
+  constructor(private twitterService: TwitterService) { }
 
   ngOnInit() {
     this.basicChart()
@@ -21,42 +21,14 @@ export class PlotlyComponent implements OnInit {
 
   basicChart() {
     //const element = this.el.nativeElement;
-
+    let trendData = this.twitterService.getTopTags();
     var trace1 = {
-      x: [1, 2, 3, 4],
-      y: [10, 15, 13, 17],
-      mode: 'markers',
-      type: 'scatter'
+      x: ['giraffes', 'orangutans', 'monkeys'],
+      y: [20, 14, 23],
+      type: 'bar'
     };
-
-    var trace2 = {
-      x: [2, 3, 4, 5],
-      y: [16, 5, 11, 9],
-      mode: 'lines',
-      type: 'scatter'
-    };
-
-    var trace3 = {
-      x: [1, 2, 3, 4],
-      y: [12, 9, 15, 12],
-      mode: 'lines+markers',
-      type: 'scatter'
-    };
-
-    var layout = {
-      title: 'Sales Growth',
-      xaxis: {
-        title: 'Year',
-        showgrid: false,
-        zeroline: false
-      },
-      yaxis: {
-        title: 'Percent',
-        showline: false
-      }
-    };
-    var data = [trace1, trace2, trace3];
+    var data = [trace1];
     //console.log("test "+element)
-    Plotly.newPlot('chart', data, layout);
+    Plotly.newPlot('chart', data);
   }
 }
